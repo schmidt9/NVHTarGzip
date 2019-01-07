de9f84d3003a6e646205224e467c147afb9bce4a
Rafal Hotlos <rafal@locuslabs.com>
2019-01-07 14:23:19 +0100

    🐛 FIX(): fix warnings in NVHGzipFile.m

--- Classes/NVHGzipFile.m
+++ Classes/NVHGzipFile.m
@@ -104,7 +104,7 @@ typedef NS_ENUM(NSInteger, NVHGzipFileErrorType)
 	// Convert source path into something a C library can handle
 	const char *sourceCString = [sourcePath cStringUsingEncoding:NSASCIIStringEncoding];
     
-	gzFile *sourceGzFile = gzopen(sourceCString, "rb");
+	gzFile sourceGzFile = gzopen(sourceCString, "rb");
     
 	unsigned int bufferLength = 1024*256;	//Thats like 256Kb
 	void *buffer = malloc(bufferLength);
@@ -222,7 +222,7 @@ typedef NS_ENUM(NSInteger, NVHGzipFileErrorType)
     // Convert destination path into something a C library can handle
     const char *destinationCString = [destinationPath cStringUsingEncoding:NSASCIIStringEncoding];
     
-    gzFile *destinationGzFile = gzopen(destinationCString, "wb");
+    gzFile destinationGzFile = gzopen(destinationCString, "wb");
     
     unsigned int bufferLength = 1024*256;	//Thats like 256Kb
     void *buffer = malloc(bufferLength);
